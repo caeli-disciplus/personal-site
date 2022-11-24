dfd185de18ef2f0b71978ed67e16e965ed823a35
Pedro Ribeiro <joaopiresribeiro@protonmail.com>
2022-11-24 22:25:08 +0100

    Created one CSS animation; Added more space between elements; Centered footer content;

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,14 @@
-const spanElement = document.getElementById('currentYear');
-const currentYear = new Date();
-spanElement.textContent = String(currentYear.getFullYear());
+/* Setting date for copyrigth */
+const spanElement = document.getElementById('currentYear')
+const currentYear = new Date()
+spanElement.textContent = String(currentYear.getFullYear())
+
+/* Animations for sections */
+const what = document.getElementById('what_we_do')
+
+window.addEventListener('scroll', () => {
+  const y = window.scrollY
+  if (y >= 220) {
+    // what.className = 'slide'
+  }
+})
