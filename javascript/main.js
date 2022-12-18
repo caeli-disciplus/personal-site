577ee2aaa0fe1afdd90ffbf73d03e301f35f2a8d
Pedro Ribeiro <joaopiresribeiro@protonmail.com>
2022-12-18 18:18:20 +0100

    Added photos; Reworked the CSS animations;

--- javascript/main.js
+++ javascript/main.js
@@ -5,10 +5,14 @@ spanElement.textContent = String(currentYear.getFullYear())
 
 /* Animations for sections */
 const what = document.getElementById('what_we_do')
+const who = document.getElementById('who_we_are')
 
 window.addEventListener('scroll', () => {
   const y = window.scrollY
-  if (y >= 220) {
-    // what.className = 'slide'
+  if (y >= 200) {
+    what.style.transform = 'translateX(-100%)'
+  }
+  if (y >= 400) {
+    who.style.transform = 'translateX(100%)'
   }
 })
